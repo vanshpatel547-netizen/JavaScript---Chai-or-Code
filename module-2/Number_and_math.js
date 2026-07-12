15f15e7e81481bff5b6e6670bd7a45fcffc0c18c
Vansh <vanshpatel547@gmail.com>
2026-07-12 17:02:46 +0530

    - Add Number_and_maths.js to module-2

---
+++ module-2/Number_and_math.js
@@ -0,0 +1,44 @@
+const score = 400;
+
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8568;
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// <------------------- Maths --------------------->
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // negative to positive & positive to positive 
+// console.log(Math.abs(4)); //  & positive to positive 
+
+// console.log(Math.round(4.6)); // default round off 
+// console.log(Math.ceil(4.2)); // select higher values 
+// console.log(Math.floor(4.2)); // select lower values 
+
+// console.log(Math.min(4,5,6,7,8)); //finds minimun value
+// console.log(Math.max(4,5,6,7,8)); // finds maximum value 
+
+console.log(Math.random()); // give values between 0 and 1 
+console.log(Math.floor((Math.random() * 10)) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // make values between 10 -> 20
+
+
+
+
+
+
+
